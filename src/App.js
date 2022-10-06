9a1a5444967a5215f9c5e021b98acec680198d51
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-06 14:51:22 -0400

    fixed layout

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+
+const App = () => {
+    return(
+        <>
+        <header></header>
+        <PortfolioContainer />
+        <footer></footer>
+        </>
+    )
+};
+
+export default App;
--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
 import NavTabs from './NavTabs';
-import Home from './pages/Home';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume'
+import Home from '../pages/Home';
+import About from '../pages/About';
+import Portfolio from '../pages/Portfolio';
+import Contact from '../pages/Contact';
+import Resume from '../pages/Resume'
 
 export default function PortfolioContainer() {
   const [currentPage, setCurrentPage] = useState('Home');
