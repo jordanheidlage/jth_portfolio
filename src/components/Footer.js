e86455c0824a7610b739aab1b080b2c1bf787cad
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-07 12:19:18 -0400

    added footer framework

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,26 @@
 import React from 'react';
+
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        <h4>
+          Made with{' '}
+          <span
+            className="emoji"
+            role="img"
+            aria-label="heart"
+            aria-hidden="false"
+          >
+            ❤️
+          </span>{' '}
+          by Jordan Heidlage, and the instructional staff at the Ohio State University College of Engineering.
+        </h4>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
