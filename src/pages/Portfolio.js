ca437a4f799316c9cc9ae1289f440baebef00e90
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-11 20:35:46 -0400

    updated portfolio with recent projects

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,6 +4,10 @@ import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import whatsForDinner from '../Assets/whatsfordinner.png'
 import socialNetworkAPI from '../Assets/socialnetwork.png'
+import writersBlock from '../Assets/writersblock.png'
+import employeeTracker from '../Assets/employeetracker.png'
+import jate from '../Assets/jate.png'
+import weather from '../Assets/weather.png'
 
 export default function Portfolio() {
 
@@ -23,39 +27,40 @@ export default function Portfolio() {
       image: socialNetworkAPI
     },
     {
-      title: '',
-      description: '',
-      repo: '',
-      deploy: '',
+      title: "Writer's Block",
+      description: 'This application allows user to publish their writings and allows the community to edit them constructively, so authors can improve their writings with community input.',
+      repo: 'https://github.com/jordanheidlage/writers_block',
+      deploy: 'https://writers-block-jh.herokuapp.com/',
+      image: writersBlock
+    },
+    {
+      title: 'Employee Tracker - SQL',
+      description: "This application allows the user to utilize .js scripts to keep an active database of their associates in the terminal: roles, salaries, direct reports, and job Id's, with the functionality to add other fields",
+      repo: 'https://github.com/jordanheidlage/employee_tracker_jh',
+      deploy: 'https://drive.google.com/file/d/1eePGCbG-aBpfLd7_9j7RSmNv6kgUlBIz/view',
+      image: employeeTracker
+    },
+    {
+      title: 'E-Commerce Backend (ORM)',
+      description: 'This was created to create a backend for an e-commerce website that uses the latest technologies so that the company can compete with other e-commerce companies. This project utilizes MySQL, npm libraries, and insomnia to perform CRUD operations to the database.',
+      repo: 'https://github.com/jordanheidlage/ecommerce_backend_jh',
+      deploy: 'https://drive.google.com/file/d/1Pi5KlrGD_QrBSJYlYuZQiJpuh_gi5Zri/view',
       image: ''
     },
     {
-      title: '',
-      description: '',
-      repo: '',
-      deploy: '',
-      image: ''
+      title: 'Text Editor - Progressive Web Application',
+      description: 'This application allows the user to edit tezt in a browser based js text editor to demonstrate Progressive Web Application functionality ',
+      repo: 'https://github.com/jordanheidlage/jth_text_editor',
+      deploy: 'https://jth-jate.herokuapp.com/',
+      image: jate
     },
     {
-      title: '',
-      description: '',
-      repo: '',
+      title: 'Weather Dashboard',
+      description: 'This application uses the OpenWeather One Call API to retrieve weather data for cities, and display the current weather, as well as temperature, wind speed, humidity, and the UV index. This application also uses localStorage to store previously searched cities. Additionally, it provides a five day forecast of the same city below containing the temperature, wind, and humidity.',
+      repo: 'https://github.com/jordanheidlage/sixth-homework',
       deploy: '',
-      image: ''
-    },
-    {
-      title: '',
-      description: '',
-      repo: '',
-      deploy: '',
-      image: ''
-    },
-    {
-      title: '',
-      description: '',
-      repo: '',
-      deploy: '',
-      image: ''
+      image: weather
+
     }
   ]
 
@@ -73,7 +78,7 @@ export default function Portfolio() {
               <Button variant="primary" href={item.repo} size="sm" target='_blank'>repo</Button>
             </Col>
             <Col md={2}>
-              <Button variant="primary" href={item.deploy} size="sm" target='_blank'>deploy</Button>
+              <Button variant="primary" href={item.deploy} size="sm" target='_blank'>Deploy/Walkthrough</Button>
             </Col>
 
           </Row>
