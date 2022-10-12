4dc7779584b830fc27c4e6c99e374728f274a419
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-11 21:08:56 -0400

    added badges to skills in about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,15 @@
 import image from "../Assets/portfoliopic.png";
 import image1 from "../Assets/family.JPG"
+import bstrapImage from "../Assets/bootstrapbadge.png"
+import expressImage from "../Assets/expressbadge.png"
+import htmlImage from "../Assets/htmlbadge.png"
+import mernBadge from "../Assets/MERNbadge.png"
+import mongoBadge from "../Assets/mongodbbadge.png"
+import nodeBadge from "../Assets/nodeBadge.png"
+import OOPBadge from "../Assets/oopBadge.jpeg"
+import sqlBadge from "../Assets/sqlbadge.webp"
+
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -17,7 +27,7 @@ export default function About() {
       </Row>
       <Row className="m-2">
         <Col md={8}>
-          <p>Jordan Heidlage is a 29 year old career Operations Manager, with years of supply chain industry experience from working in multiple fortune 500 companies. Jordan currently resides in Cincinnati, Ohio with his fiance, Rachel, and Golden Retriever, Kevin. Jordan was born, and raised in Ross, Ohio, just shortly outside of Cincinnati. In grade school, Jordan distinguished himself as a local spelling bee champion, as well as an active competitor in local skateboard competitions, while maintaining several part time jobs. In college, Jordan attended Harding University in Searcy, Arkansas his freshman year where he played Lacrosse. Jordan later transferred to Miami University (Oh), where he obtained a B.A. in Biology, and a minor in Anthropology, while also maintaining a full time leadership position at the campus recreational center. Upon graduation, Jordan completed a Management Training Program with Kohl's Department Stores, Inc, where he discovered his passion for data analytics, and UI. Jordan spent 2 years as an Operations/HR Manager within the Kohl's Corporation, before accepting a role as an Operations Manager with Amazon.com LLC in Hebron, Kentucky to continue to challenge his skills in data analytics, and leadership. In his free time, Jordan loves taking Kevin on hikes, skateboarding, lifting weights, long bikes rides, home improvement projects, and spending time with loved ones. </p>
+          <p>Jordan Heidlage is a 29 year old career Fulfillment/Operations Manager, with years of supply chain industry experience from working in multiple fortune 500 companies. Jordan currently resides in Cincinnati, Ohio with his girlfriend, Rachel, and Golden Retriever, Kevin. Jordan was born, and raised in Ross, Ohio, just shortly outside of Cincinnati. In grade school, Jordan distinguished himself as a local spelling bee champion, as well as an active competitor in local skateboard competitions, and enging in varsity Track, Cross Country, and Swimming, while maintaining several part time jobs. In college, Jordan attended Harding University in Searcy, Arkansas his freshman year where he played Lacrosse. Jordan later transferred to Miami University (Oh), where he obtained a B.A. in Biology, and a minor in Anthropology, while also maintaining a full time leadership position at the campus recreational center. Upon graduation, Jordan completed an extensive Management Training Program with Kohl's Department Stores, Inc, where he discovered his passion for data analytics, supply chain, and UI. Jordan spent 2 years as an Operations/HR Manager within the Kohl's Corporation, before accepting a role as an Operations Manager with Amazon.com LLC in Hebron, Kentucky to continue to challenge his skills in data analytics, and leadership. In his free time, Jordan loves taking Kevin on hikes, skateboarding, lifting weights, long bikes rides, home improvement projects, camping, record shopping, exploring new music, and spending time with loved ones. </p>
         </Col>
         <Col md={4}>
           <img src={image1} width={100} className="brand-image" />
@@ -27,15 +37,40 @@ export default function About() {
         <Col>
           <p>Skills:</p>
           <Row>
-            <Col md={2}>
+            <Col md={1}>
+              <img src={bstrapImage} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={expressImage} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={htmlImage} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={mernBadge} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={mongoBadge} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={nodeBadge} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={OOPBadge} width={50} />
+            </Col>
+            <Col md={1}>
+              <img src={sqlBadge} width={50} />
+            </Col>
+            <Col md={1}>
               <img src={image} width={50} />
             </Col>
-            <Col md={2}>
+            <Col md={1}>
               <img src={image} width={50} />
             </Col>
-            <Col md={2}>
+            <Col md={1}>
               <img src={image} width={50} />
             </Col>
+
           </Row>
         </Col>
       </Row>
