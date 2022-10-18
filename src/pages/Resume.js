9fcdb5b3e6903b42f0abfd3050bf6fa7b547eee9
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-18 15:49:22 -0400

    fixed resume to downloadable form

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -6,7 +6,9 @@ export default function Resume() {
     <div>
       <h1>Resume</h1>
       <p>
-<script src='/resume.pdf'></script>      </p>
+      <a href={process.env.PUBLIC_URL + "/resume.pdf"} download>Resume</a></p>
+      <iframe src="https://docs.google.com/document/d/1VOYFNJpVj3VJ7DCPSlmBxOoitLrqptNf/edit?usp=sharing&ouid=106342406943723089611&rtpof=true&sd=true" title="Resume"></iframe>
+
     </div>
   );
 }
