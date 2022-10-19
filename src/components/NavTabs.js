4faa5a0e15097ea8b2ee067b9d0dabb2e4e73305
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-10-19 13:32:45 -0400

    changing navtab styling

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,7 +4,7 @@ import React from 'react';
 // We assign them to their own variable names
 function NavTabs({ currentPage, handlePageChange }) {
   return (
-    <ul className="nav nav-tabs">
+    <ul className="nav nav-tabs justify-content-center">
       <li className="nav-item">
         <a
           href="#home"
