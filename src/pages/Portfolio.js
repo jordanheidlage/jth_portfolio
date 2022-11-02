bd83f6ebe94c88143e4ca584e4b4bc7196c8920b
Jordan Heidlage <jordanheidlage@jordans-air.lan>
2022-11-02 15:44:29 -0400

    updated portfolio

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -19,39 +19,47 @@ export default function Portfolio() {
   const projectArray = [
     {
       title: 'Explorar',
-      description: "This application utilizes the full MERN stack to allow users to create an account, sign in, and save travel preferences for up to 6 different countries",
+      description: "This application utilizes the full MERN stack to allow users to create an account, sign in, and save travel preferences for up to 6 different countries.",
       repo: 'https://github.com/jordanheidlage/Explorar',
       deploy: 'https://explorar-project3.herokuapp.com/',
       image: explorar
     },
     {
+      title: 'Social Network API - MongoDB ',
+      description: 'This application was created to utilize MongoDB database management, and Insomnia to test the correct functionality of API routes for a social networking site. You can create users, share thoughts, react to friends thoughts, and create a list of friends.',
+      repo: 'https://github.com/jordanheidlage/social_network_API_jheidlage',
+      deploy: 'https://drive.google.com/file/d/1UaWeDR4srUhVB8_5MJwDURKwLGEqeQtO/view',
+      walkthrough: 'https://drive.google.com/file/d/1UaWeDR4srUhVB8_5MJwDURKwLGEqeQtO/view',
+      image: socialNetworkAPI
+    },
+    {
+      title: 'Text Editor - Progressive Web Application',
+      description: 'This application allows the user to edit text in a browser based js text editor, with offline capability to demonstrate Progressive Web Application functionality ',
+      repo: 'https://github.com/jordanheidlage/jth_text_editor',
+      deploy: 'https://jth-jate.herokuapp.com/',
+      image: jate
+    },
+    {
       title: 'Whats for dinner',
-      description: "This application was created to generate a random food and a random cocktail from two different API's",
+      description: "This application was created to generate a random food, and a random cocktail from two different API's, saving them in local storage for the user to call upon later",
       repo: 'https://github.com/jordanheidlage/whatsForDinner',
       deploy: 'https://jordanheidlage.github.io/whatsForDinner/',
       image: whatsForDinner
     },
     {
-      title: 'Note Taker',
-      description: "This application allows the user to take notes, and save them as persistent data to be reviewed later",
-      repo: 'https://github.com/jordanheidlage/note_taker_jth',
-      deploy: 'https://note-takerjh.herokuapp.com/',
-      image: notetaker
+      title: 'E-Commerce Backend (ORM)',
+      description: 'This was created to create a backend for an e-commerce website that uses the latest technologies so that the company can compete with other e-commerce companies. This project utilizes MySQL, npm libraries, and insomnia to perform CRUD operations to the database to manage/view categories, tags, and products.',
+      repo: 'https://github.com/jordanheidlage/ecommerce_backend_jh',
+      deploy: 'https://drive.google.com/file/d/1Pi5KlrGD_QrBSJYlYuZQiJpuh_gi5Zri/view',
+      walkthrough: 'https://drive.google.com/file/d/1Pi5KlrGD_QrBSJYlYuZQiJpuh_gi5Zri/view',
+      image: ecommerce
     },
     {
       title: 'Team Profile Generator',
-      description: "This application allows the user to take notes, and save them as persistent data to be reviewed later",
+      description: "This application allows the user to utilize node commands to generate an HTML page displaying profiles for each member of their team.",
       repo: 'https://github.com/jordanheidlage/team_profile_generator',
       deploy: 'https://drive.google.com/file/d/1PM25f7arPMmFbAC2dWtiPoQN4OEoV4hO/view?usp=sharing',
       image: teamgenerator
-    },
-    {
-      title: 'Social Network API - MongoDB ',
-      description: 'This application was created to utilize MongoDB database management, and Insomnia to test the correct functionality of API routes',
-      repo: 'https://github.com/jordanheidlage/social_network_API_jheidlage',
-      deploy: 'https://drive.google.com/file/d/1UaWeDR4srUhVB8_5MJwDURKwLGEqeQtO/view',
-      walkthrough: 'https://drive.google.com/file/d/1UaWeDR4srUhVB8_5MJwDURKwLGEqeQtO/view',
-      image: socialNetworkAPI
     },
     {
       title: "Writer's Block",
@@ -70,28 +78,19 @@ export default function Portfolio() {
       image: employeeTracker
     },
     {
-      title: 'E-Commerce Backend (ORM)',
-      description: 'This was created to create a backend for an e-commerce website that uses the latest technologies so that the company can compete with other e-commerce companies. This project utilizes MySQL, npm libraries, and insomnia to perform CRUD operations to the database.',
-      repo: 'https://github.com/jordanheidlage/ecommerce_backend_jh',
-      deploy: 'https://drive.google.com/file/d/1Pi5KlrGD_QrBSJYlYuZQiJpuh_gi5Zri/view',
-      walkthrough: 'https://drive.google.com/file/d/1Pi5KlrGD_QrBSJYlYuZQiJpuh_gi5Zri/view',
-      image: ecommerce
-    },
-    {
-      title: 'Text Editor - Progressive Web Application',
-      description: 'This application allows the user to edit tezt in a browser based js text editor to demonstrate Progressive Web Application functionality ',
-      repo: 'https://github.com/jordanheidlage/jth_text_editor',
-      deploy: 'https://jth-jate.herokuapp.com/',
-      image: jate
-    },
-    {
       title: 'Weather Dashboard',
       description: 'This application uses the OpenWeather One Call API to retrieve weather data for cities, and display the current weather, as well as temperature, wind speed, humidity, and the UV index. This application also uses localStorage to store previously searched cities. Additionally, it provides a five day forecast of the same city below containing the temperature, wind, and humidity.',
       repo: 'https://github.com/jordanheidlage/sixth-homework',
       deploy: 'https://jordanheidlage.github.io/weather_dashboard_jth/',
       image: weather
-
-    }
+    },
+    {
+      title: 'Note Taker',
+      description: "This application allows the user to take notes, and save them as persistent data to be reviewed later",
+      repo: 'https://github.com/jordanheidlage/note_taker_jth',
+      deploy: 'https://note-takerjh.herokuapp.com/',
+      image: notetaker
+    },
   ]
 
   return (
